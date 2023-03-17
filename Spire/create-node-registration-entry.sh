be85e33f39d839bd594eb955d4fa579231f1fe99
jch <jch@hpe.com>
2023-03-17 14:30:11 +0530

    Added spiffe ID creation

--- Spire/create-node-registration-entry.sh
+++ Spire/create-node-registration-entry.sh
@@ -6,11 +6,43 @@ bb=$(tput bold)
 nn=$(tput sgr0)
 
 
+
+register() {
+    kubectl exec -n spire spire-server-0 -c spire-server -- /opt/spire/bin/spire-server entry create $@
+}
+
 echo "${bb}Creating registration entry for the node...${nn}"
-kubectl exec -n spire spire-server-0 -- \
+register \
     /opt/spire/bin/spire-server entry create \
     -node  \
     -spiffeID spiffe://example.org/ns/spire/sa/spire-agent \
     -selector k8s_sat:cluster:kind-spire \
     -selector k8s_sat:agent_ns:spire \
     -selector k8s_sat:agent_sa:spire-agent
+
+echo "${bb}Creating registration entry for the registry - auth-server...${nn}"
+register \
+    -parentID spiffe://example.org/ns/spire/sa/spire-agent \
+    -spiffeID spiffe://example.org/ns/gx-lab/sa/default/gx-registry-server-main \
+    -selector k8s:ns:default \
+    -selector k8s:sa:default \
+    -selector k8s:pod-label:app:backend \
+    -selector k8s:container-name:envoy
+
+echo "${bb}Creating registration entry for the compliance - auth-server...${nn}"
+register \
+    -parentID spiffe://example.org/ns/spire/sa/spire-agent \
+    -spiffeID spiffe://example.org/ns/gx-lab/sa/default/gx-compliance-server-main \
+    -selector k8s:ns:gx-lab \
+    -selector k8s:sa:default \
+    -selector k8s:pod-label:app:gx-compliance-main \
+    -selector k8s:container-name:envoy
+
+ echo "${bb}Creating registration entry for the workload - auth-server...${nn}"
+register \
+    -parentID spiffe://example.org/ns/spire/sa/spire-agent \
+    -spiffeID spiffe://example.org/ns/gx-lab/sa/default/gx-workload \
+    -selector k8s:ns:gx-lab \
+    -selector k8s:sa:default \
+    -selector k8s:pod-label:app:gx-workload \
+    -selector k8s:container-name:envoy
