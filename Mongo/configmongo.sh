e1371732eacbe68e861baa9b69dfbc3f7351b506
Jayasheel <jaysheelch@gmail.com>
2023-03-28 06:12:07 +0000

    Changes for ocp cluster

--- Mongo/configmongo.sh
+++ Mongo/configmongo.sh
@@ -1,5 +1,5 @@
-mongopod=`sudo kubectl get pods -l io.kompose.service=mongo -n gx-lab -o json | jq .items[0].metadata.name | sed 's/"//g'`
-sudo kubectl exec -it ${mongopod} -n gx-lab -- mongosh << _EOT_
+mongopod=`sudo kubectl get pods -l io.kompose.service=mongo -n gxdch -o json | jq .items[0].metadata.name | sed 's/"//g'`
+sudo kubectl exec -it ${mongopod} -n gxdch -- mongosh << _EOT_
 use trust-registry
 db.createUser({user:"trustuser",pwd:"password",roles:["dbOwner"]})
 db.getName()
